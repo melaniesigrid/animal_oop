849e8e58bcda16be0091748e0d97cfcd40030729
Melanie Arellano <melaniesigrid@protonmail.com>
2022-04-13 15:34:37 -0600

    Add files that exemplify association, aggregation and composition

--- Animal.rb
+++ Animal.rb
@@ -2,13 +2,19 @@ require "./remover.rb"
 require "./foods.rb"
 
 class Animal
-  attr_accessor :name, :id, :type, :number_of_legs
+  attr_accessor :name, :id, :type, :number_of_legs, :owner, :visits
   def initialize(type, number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
     @type = type
     @liked_food = NoFood.new()
+    @visits = []
+  end
+
+  def owner=(owner)
+    @owner = owner
+    owner.animals.push(self) unless owner.animals.include?(self)
   end
 
   def likes_food?(food)
